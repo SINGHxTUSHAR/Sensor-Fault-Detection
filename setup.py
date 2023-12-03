871571c05d5caa16d85c5f8ed090221daaa7405b
SINGHxTUSHAR <tusharsinghrawat.delhi@gmail.com>
2023-12-03 19:41:43 +0530

    modified

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+        return requirements
+
 
 setup(
-    name="src",
-    version="0.0.1",
-    author="Tushar Singh",
-    author_email="tusharsinghrawat.delhi@gmail.com",
-    packages=find_packages(),
-    install_requires=[],
+    name='Sensor-Fault-Detection',
+    version='0.0.1',
+    author='TUSHAR SINGH',
+    author_email='tusharsinghrawat.delhi@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+
 )
